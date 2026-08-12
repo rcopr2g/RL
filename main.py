011cd0b2e81ddf9a6829f9aef79179bec089524b
Rcopr2g <rcopr2g@gmail.com>
2026-08-12 22:21:24 +0800

    feat(cartpole): add initial demo and policy baselines
    - record results for random and untrained Q-network policies
    - include baseline  comparison plots

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import torch, torch.nn as nn
 
 plt.rcParams.update({"font.size": 14, "figure.dpi": 110})
 
-env = gym.make("CartPole-v1")
-obs, _ = env.reset()
-# print(obs)
-# print(obs.dtype, env.observation_space.low, env.observation_space.high)
+seed = 42
 
+env = gym.make("CartPole-v1")
+obs, _ = env.reset(seed=seed)
+env.action_space.seed(seed)
+
+torch.manual_seed(seed)
 net = nn.Sequential(nn.Linear(4,64), nn.ReLU(), nn.Linear(64,2))
 
-n = 1
+n = 300
 sum = 0
 vec = []
 for i in range(n):
@@ -22,7 +24,7 @@ for i in range(n):
     while not done:
         q_val = net(
             torch.tensor(obs, dtype=torch.float32).unsqueeze(0)
-            ) # 神秘batch
+            ) # 神秘batch, 为了防止后续argmax处dim不一致, 所以加上unsqueeze
         act = q_val.argmax(dim=1).item()
         next_obs, reward, terminated, truncated, _ = env.step(act)
         obs = next_obs
@@ -31,9 +33,11 @@ for i in range(n):
     sum += cnt
     vec.append(cnt)
 
+np.savez("./data/init.npz", vec=vec, seed=seed)
+
 fig, axes = plt.subplots(1, 1, figsize=(10, 7), sharey=True, sharex=True)
 # ax = axes[0]
 axes.plot([sum/n for _ in range(n)])
 axes.plot(vec)
-# fig.savefig('./figs/random.png', dpi=150, bbox_inches='tight')
+fig.savefig('./figs/init.png', dpi=300, bbox_inches='tight')
 plt.show()
