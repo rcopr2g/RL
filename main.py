287227bad75ce3c7e89d05c1b19a4042a078b2a7
Rcopr2g <rcopr2g@gmail.com>
2026-08-14 15:37:48 +0800

    feat(cartpole): add epsilon-greedy exploration and replay buffer
    - select random or greedy actions based on epsilon
    - store transitions in a bounded replay buffer

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import gymnasium as gym, numpy as np
 import numpy as np
 import matplotlib.pyplot as plt
 import torch, torch.nn as nn
+from collections import deque
+import random
 
 plt.rcParams.update({"font.size": 14, "figure.dpi": 110})
 
@@ -14,9 +16,11 @@ env.action_space.seed(seed)
 torch.manual_seed(seed)
 net = nn.Sequential(nn.Linear(4,64), nn.ReLU(), nn.Linear(64,2))
 
+eps = 1.0
 n = 300
 sum = 0
 vec = []
+buffer = deque(maxlen=10_100)
 for i in range(n):
     obs, _ = env.reset()
     done = False
@@ -25,11 +29,13 @@ for i in range(n):
         q_val = net(
             torch.tensor(obs, dtype=torch.float32).unsqueeze(0)
             ) # 神秘batch, 为了防止后续argmax处dim不一致, 所以加上unsqueeze
-        act = q_val.argmax(dim=1).item()
+        act = env.action_space.sample() if np.random.rand() < eps \
+            else q_val.argmax(dim=1).item()
         next_obs, reward, terminated, truncated, _ = env.step(act)
-        obs = next_obs
         done = terminated or truncated
+        buffer.append((obs, act, reward, next_obs, terminated, truncated))
         cnt += 1
+        obs = next_obs
     sum += cnt
     vec.append(cnt)
 
